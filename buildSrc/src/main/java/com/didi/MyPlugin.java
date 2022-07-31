78607295ed4672a01160f17ab9c1b7d528ec2907
Bo Zhang <bo@gradle.com>
2022-08-01 06:38:39 +0800

    Init commit

---
+++ buildSrc/src/main/java/com/didi/MyPlugin.java
@@ -0,0 +1,13 @@
+package com.didi;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MyPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.afterEvaluate(p -> {
+
+        });
+    }
+}
